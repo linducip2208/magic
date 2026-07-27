2b85ae70c1797ae8fa4c14b009286293f0892bdc
linducip2208 <lindu@act.co.id>
2026-07-27 14:58:47 +0700

    fix: include Vite build assets for shared hosting deployment

---
+++ public/build/assets/app-qzOjnZjK.js
@@ -0,0 +1,1 @@
+import"./app-D4Q-0Xy5.js";import"./livewire.esm-CJWicNdV.js";import"./fetch-SQBjdvMm.js";import"./clipboard-CBCTDmBy.js";import"./lib.modern-DJaFKjpz.js";import"./_commonjsHelpers-Cpj98o6Y.js";
